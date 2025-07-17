894445079a3c864a191f0ac633b1f705ca97ab62
Sarmad Cheema <sarmadc45@gmail.com>
2025-07-17 17:44:24 +0000

    Add eslint directive to suppress unused variable warning for eventType in HappeningNowIndicator and ModalHappeningNow components

--- src/components/HappeningNowIndicator.tsx
+++ src/components/HappeningNowIndicator.tsx
@@ -23,7 +23,7 @@ export default function HappeningNowIndicator({
   startTime, 
   endTime, 
   title,
-  eventType = '',
+  eventType = '', // eslint-disable-line @typescript-eslint/no-unused-vars
   className = '',
   size = 'md' 
 }: HappeningNowIndicatorProps) {
@@ -328,7 +328,7 @@ export function ModalHappeningNow({
   startTime, 
   endTime, 
   title, 
-  eventType 
+  eventType = '' // eslint-disable-line @typescript-eslint/no-unused-vars
 }: { 
   startTime: string; 
   endTime?: string | null; 
